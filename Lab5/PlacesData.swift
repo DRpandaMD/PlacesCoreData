45568b0e31e17eb6aaf8f96adbc23bcbf576f512
DRpandaMD <michael.zarate.ce@gmail.com>
2015-10-18 14:33:46 -0700

    Places with Core Data
    Places app using permanent data storage

---
+++ Lab5/PlacesData.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesData.swift
+//  Lab5
+//
+//  Created by Michael Zarate on 10/2/15.
+//  Copyright (c) 2015 Michael Zarate. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PlacesData: NSManagedObject {
+
+    @NSManaged var name: String
+    @NSManaged var placesDisc: String
+
+}
--- Lab5/ViewController.swift
+++ Lab5/ViewController.swift
@@ -7,9 +7,29 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    var maxPlace = 0
 
+    var index = 0
+    
+   
+    @IBOutlet weak var nameTxtField: UITextField!
+    @IBOutlet weak var recordTxtView: UITextView!
+    @IBOutlet weak var numRecordTxtField: UITextField!
+    
+    @IBOutlet weak var placeTxtField: UITextField!
+    
+    @IBOutlet weak var dataOutTxtView: UITextView!
+    
+    
+    let insertContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    
+    var viewContext: NSManagedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +39,101 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func addButton(sender: UIButton) {
+        
+        let ent = NSEntityDescription.entityForName("PlacesData", inManagedObjectContext: self.insertContext!)
+        let newItem = PlacesData(entity: ent!, insertIntoManagedObjectContext: self.insertContext!)
+        newItem.name = nameTxtField.text
+        newItem.placesDisc = recordTxtView.text
+        self.insertContext?.save(nil)
+        
+    }
+    
+    @IBAction func prevButton(sender: UIButton) {
+        //create fetch request
+        
+        let fetchRequest = NSFetchRequest(entityName: "PlacesData")
+        
+        let maxItems = getMaxPlaces()
+        //perform fetch request
+        
+        
+        
+        if(index >= 0 && index <= maxItems)
+        {
+            if(index > 0)
+            {
+                index--
+            }
+            
+            if  let fetchResults = viewContext.executeFetchRequest(fetchRequest, error: nil) as? [PlacesData]
+            {
+            
+                let list = fetchResults[index]
+                
+                self.placeTxtField.text = list.name
+                self.dataOutTxtView.text = list.placesDisc
+            
+            }
+            self.numRecordTxtField.text =  "\(index + 1) of \(maxItems)"
+            
+            
+        }
+        else{
+           return
+        }
+        
+        
+    }
+   
+    @IBAction func nextButton(sender: UIButton) {
+        
+        let fetchRequest = NSFetchRequest(entityName: "PlacesData")
+        
+        let maxItems = getMaxPlaces();
+        
+        if(index < maxItems && index >= 0)
+        {
+        
+            if  let fetchResults = viewContext.executeFetchRequest(fetchRequest, error: nil) as? [PlacesData]
+            {
+            
+                let list = fetchResults[index]
+            
+                self.placeTxtField.text = list.name
+                self.dataOutTxtView.text = list.placesDisc
+            }
+            
+            self.numRecordTxtField.text =  "\(index + 1) of \(maxItems)"
+            index++
+            
+        }
+        else
+        {
+            return
+        }
+        
+    }
+    
+    func getMaxPlaces() -> Int
+    {
+        let fetchRequest = NSFetchRequest(entityName: "PlacesData")
+        
+        //perform fetch request
+        
+        if  let fetchResults = viewContext.executeFetchRequest(fetchRequest, error: nil) as? [PlacesData]
+        {
+            maxPlace = fetchResults.count
+            
+        }
+
+        
+        return maxPlace
+    }
+
+    
 
 
 }
